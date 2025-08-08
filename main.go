b1b89d709fa40cca2e4dca1c96e49bb15341bcd2
Drishti Aggarwal <espazeindia@gmail.com>
2025-08-08 14:32:56 +0530

    retry deployment

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 )
 
 func SecurityHeaders() gin.HandlerFunc {
@@ -24,10 +23,10 @@ func SecurityHeaders() gin.HandlerFunc {
 
 func main() {
 	// 📦 Load environment variables
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("❌ Error loading .env file")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("❌ Error loading .env file")
+	// }
 
 	port := os.Getenv("PORT")
 	if port == "" {
