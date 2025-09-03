ade3c57035cbc1053d86abfaec2c7267981923b3
Drishti Aggarwal <espazeindia@gmail.com>
2025-09-03 16:37:22 +0530

    changes in onboarding seller

--- domain/entities/onboarding.go
+++ domain/entities/onboarding.go
@@ -3,9 +3,9 @@ package entities
 type SellerBasicDetail struct {
 	Name        string `json:"name" bson:"name"`
 	ShopAddress string `json:"address" bson:"address"`
-	Gstin       string `json:"Gstin" bson:"Gstin"`
+	Gstin       string `json:"gstin" bson:"Gstin"`
 	Pan         string `json:"pan" bson:"pan"`
 	CompanyName string `json:"companyName" bson:"companyName"`
-	ShopName    string `json:"ShopName" bson:"ShopName"`
+	ShopName    string `json:"shopName" bson:"ShopName"`
 	PIN         int    `json:"pin" bson:"pin,min=6"`
 }
